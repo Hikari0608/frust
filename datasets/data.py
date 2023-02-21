d366067ae4bebef8f4743615a4cdd5263fb20916
Hikari0608 <emtttt@foxmail.com>
2023-02-21 15:36:54 +0800

    fix bug

--- datasets/data.py
+++ datasets/data.py
@@ -6,13 +6,19 @@ import numpy as np
 
 def load_image(pth, name):
     file = tf.io.read_file(os.path.join(pth, name))
-    img = tf.io.decode_jpeg(file)
-    return np.array(img).tobytes()
+    if name.lower().endswith('.png'):
+        img = tf.io.decode_png(file, channels=3)
+    else:
+        img = tf.io.decode_jpeg(file, channels=3)
+    return np.array(img)
 
 # Define the features of the dataset
 image_feature_description = {
     'image': tf.io.FixedLenFeature([], tf.string),
-    'label': tf.io.FixedLenFeature([], tf.int64),
+    'height': tf.io.FixedLenFeature((), tf.int64),
+    'width': tf.io.FixedLenFeature((), tf.int64),
+    'channels': tf.io.FixedLenFeature((), tf.int64),
+    'label': tf.io.FixedLenFeature([], tf.int64)
 }
 
 # Create a TFRecord file writer
@@ -32,10 +38,14 @@ for label in os.listdir(dataset_dir):
         if not(label in label_map):
             label_map[label] = len(label_map)
         label_int = label_map[label]
+        [h,w,c] = image.shape
 
         # Serialize the data
         example = tf.train.Example(features=tf.train.Features(feature={
-            'image': tf.train.Feature(bytes_list=tf.train.BytesList(value=[image])),
+            'image': tf.train.Feature(bytes_list=tf.train.BytesList(value=[image.tobytes()])),
+            'height': tf.train.Feature(int64_list=tf.train.Int64List(value=[h])),
+            'width': tf.train.Feature(int64_list=tf.train.Int64List(value=[w])),
+            'channels': tf.train.Feature(int64_list=tf.train.Int64List(value=[c])),
             'label': tf.train.Feature(int64_list=tf.train.Int64List(value=[label_int])),
         }))
         serialized_example = example.SerializeToString()
--- train.py
+++ train.py
@@ -7,11 +7,26 @@ from tensorflow._api.v2 import io
 
 from models import VGG
 
+
+# Create a description of the features.
+image_feature_description = {
+    'image': tf.io.FixedLenFeature([], tf.string),
+    'label': tf.io.FixedLenFeature([], tf.int64)
+}
+
+def _parse_image_function(example_proto):
+  # Parse the input tf.Example proto using the dictionary above.
+  return tf.io.parse_single_example(example_proto, image_feature_description)
+
+raw_image_dataset = tf.data.TFRecordDataset('./datasets/fruits.tfrecord/')
+parsed_image_dataset = raw_image_dataset.map(_parse_image_function)
+for image_features in parsed_image_dataset:
+  image_raw = image_features['image_raw'].numpy()
+  display.display(display.Image(data=image_raw))
+
 model = VGG(weights='imagenet', include_top=True)
 
 # 使用预训练模型进行预测
-img = load_img('example.jpg', target_size=(224, 224))
-img = img_to_array(img)
 img = np.expand_dims(img, axis=0)
 img = preprocess_input(img)
 predictions = model.predict(img)
